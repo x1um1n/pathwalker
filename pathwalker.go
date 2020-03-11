6e139d77a9a795fd920774367eb24633d6bfce0c
matt-beaney <matt.beaney@autotrader.co.uk>
2020-03-11 19:24:32 +0000

    move location functions to dedicated file

--- pathwalker.go
+++ pathwalker.go
@@ -7,8 +7,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -19,8 +17,6 @@ import (
 
 	"github.com/heptiolabs/healthcheck"
 	"github.com/x1um1n/checkerr"
-
-	"github.com/gosexy/exif"
 )
 
 // Routes creates a router and calls routes.Routes to add in the api routes
@@ -62,61 +58,6 @@ func makeImageCSL(ims []Image) (s string) {
 			s += img.ImageID + ","
 		}
 	}
-	return
-}
-
-// getLocation opens a local file, extracts the lat/long from EXIF data & returns it as a coordinate struct
-func getLocation(path string) (c Coordinate, e error) {
-	log.Println("Reading EXIF data from " + path)
-	parser := exif.New()
-	e = parser.Open(path)
-	if !checkerr.Check(e, "Error parsing EXIF data from ", path) {
-		if parser.Tags["Latitude"] != "" {
-			if parser.Tags["North or South Latitude"] != "" {
-				if parser.Tags["Longitude"] != "" {
-					if parser.Tags["East or West Longitude"] != "" {
-						c = parseCoord(parser.Tags["Latitude"], parser.Tags["North or South Latitude"], parser.Tags["Longitude"], parser.Tags["East or West Longitude"])
-					} else {
-						log.Println("Location either missing or incomplete in EXIF data")
-					}
-				} else {
-					log.Println("Location either missing or incomplete in EXIF data")
-				}
-			} else {
-				log.Println("Location either missing or incomplete in EXIF data")
-			}
-		} else {
-			log.Println("Location either missing or incomplete in EXIF data")
-		}
-	}
-	return
-}
-
-// parseCoordString takes an EXIF lat/long string and converts it to a coordinate
-func parseCoordString(val string) float64 {
-	chunks := strings.Split(val, ",")
-	hours, _ := strconv.ParseFloat(strings.TrimSpace(chunks[0]), 64)
-	minutes, _ := strconv.ParseFloat(strings.TrimSpace(chunks[1]), 64)
-	seconds, _ := strconv.ParseFloat(strings.TrimSpace(chunks[2]), 64)
-
-	return hours + (minutes / 60) + (seconds / 3600)
-}
-
-// parseCoord takes location values from EXIF data and returns a Coordinate struct
-func parseCoord(latVal, latRef, lngVal, lngRef string) (c Coordinate) {
-	lat := parseCoordString(latVal)
-	lng := parseCoordString(lngVal)
-
-	if latRef == "S" { // N is "+", S is "-"
-		lat *= -1
-	}
-
-	if lngRef == "W" { // E is "+", W is "-"
-		lng *= -1
-	}
-
-	c = Coordinate{lat, lng}
-
 	return
 }
 
